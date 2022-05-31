1a5d6804f724fba4e3309c8de8f23c9a4b39363b
hung dao <hungpepi2001@gmail.com>
2022-05-31 11:04:04 +0700

    get error

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,25 @@
+import db from('../models/index')
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
-}
+let getHomePage =async (req, res) => {
+    try {
+        const data =  await db.User.findAll()
+        console.log("🚀 ~ file: homeController.js ~ line 6 ~ getHomePage ~ data", data)
+        return res.render("homepage.ejs");
+    } catch (error) {
+    console.log("🚀 ~ file: homeController.js ~ line 7 ~ getHomePage ~ error", error)
+    }
 
+};
+  
 let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
-}
+  return res.render("test/about.ejs");
+};
 
 // object: {
 //     key: '',
 //     value: ''
 // }
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage
-}
+  getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+  };
