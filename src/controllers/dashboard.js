cf4dfd73ba46857f4e67a1b93e14ee958c2c5cb1
hung dao <hungpepi2001@gmail.com>
2023-04-23 18:47:35 +0700

    fix api dashboard

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -3,22 +3,118 @@ import Specialty from "../models/specialty";
 import User from "../models/user";
 import Handbook from "../models/handbook";
 import Patient from "../models/patient";
+import Schedule from "../models/schedule";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 
+// exports.getAllCount = catchAsyncErrors(async (req, res, next) => {
+//   const clinic = await Clinic.count();
+//   const user = await User.count();
+//   const specialty = await Specialty.count();
+//   const patient = await Patient.count();
+//   const [schedule] = await Schedule.aggregate([
+//     {
+//       $unwind: "$schedule",
+//     },
+//     { $match: { "schedule.status": "Hoàn thành" } },
+//     { $group: { _id: "$schedule.status", count: { $sum: 1 } } },
+//   ]);
+//   res.status(200).json({
+//     count: {
+//       clinic,
+//       user: user - 1,
+//       specialty,
+//       patient,
+//       schedule: schedule.count,
+//     },
+//     success: true,
+//   });
+// });
+
 exports.getAllCount = catchAsyncErrors(async (req, res, next) => {
-  const clinic = await Clinic.count();
-  const user = await User.count();
-  const specialty = await Specialty.count();
-  const patient = await Patient.count();
+  const promises = [
+    Clinic.count(),
+    User.count(),
+    Patient.count(),
+    Schedule.aggregate([
+      {
+        $unwind: "$schedule",
+      },
+      { $match: { "schedule.status": "Hoàn thành" } },
+      { $group: { _id: "$schedule.status", count: { $sum: 1 } } },
+    ]),
+  ];
+
+  const [clinic, user, patient, schedule] = await Promise.all(promises);
 
   res.status(200).json({
     count: {
       clinic,
       user: user - 1,
-      specialty,
       patient,
+      schedule: schedule[0].count,
     },
+    success: true,
+  });
+});
+// lấy tất cả lịch khám
+exports.getAllMedicalHistory = catchAsyncErrors(async (req, res, next) => {
+  const list = await Schedule.aggregate([
+    {
+      $unwind: "$schedule",
+    },
+    {
+      $match: {
+        "schedule.user.email": { $ne: null },
+        "schedule.status": "Hoàn thành",
+      },
+    },
+    {
+      $project: {
+        date: 1,
+        "doctor.name": 1,
+        "packet.name": 1,
+        "detail.price.name": 1,
+        "detail.payment.name": 1,
+        "detail.note": 1,
+        schedule: 1,
+      },
+    },
+  ]);
+  res.status(200).json({
+    list,
+    success: true,
+  });
+});
+
+// lấy tất cả người dùng
+exports.getAllPatientAccount = catchAsyncErrors(async (req, res, next) => {
+  const patient = await Patient.find(
+    {},
+    "name email gender dateOfBirth phone address.detail"
+  );
+  res.status(200).json({
+    list: patient,
+    success: true,
+  });
+});
+
+exports.getAllDoctorAccount = catchAsyncErrors(async (req, res, next) => {
+  const doctor = await User.find(
+    {
+      roleId: { $not: { $regex: "R0" } },
+    },
+    "name email gender dateOfBirth phone address.detail detail.clinic.name detail.specialty.name detail.position.name"
+  );
+  res.status(200).json({
+    list: doctor,
+    success: true,
+  });
+});
+exports.getAllLocationClinic = catchAsyncErrors(async (req, res, next) => {
+  const list = await Clinic.find({}, "name address.lat address.lng");
+  res.status(200).json({
+    list,
     success: true,
   });
 });
--- src/route/user.js
+++ src/route/user.js
@@ -15,18 +15,36 @@ import {
   outStandingDoctor,
 } from "../controllers/user.js";
 
-import { getAllCount } from "../controllers/dashboard";
+import {
+  getAllCount,
+  getAllMedicalHistory,
+  getAllDoctorAccount,
+  getAllPatientAccount,
+  getAllLocationClinic,
+} from "../controllers/dashboard";
 
 router.route("/create-user").post(isAuthunticatedUser, create);
 router.route("/delete-user").delete(isAuthunticatedUser, remove);
 router.route("/update-user").put(isAuthunticatedUser, update);
 router.route("/get-user").get(isAuthunticatedUser, getSingle);
 router.route("/get-all-user").get(isAuthunticatedUser, getAll);
-router.route("/get-all-count").get(getAllCount);
 router.route("/get-all-user-home").get(getAllHomePatient);
 router.route("/get-user-by-specialty-home").get(getAllDoctorBySpecialtyHome);
 router.route("/get-user-by-province-home").get(getAllDoctorByProvince);
 router.route("/suggest-doctor-recent").get(suggestDoctorRecent);
 router.route("/get-outstading-doctor").get(outStandingDoctor);
-
+// dashboard route
+router.route("/get-all-count").get(isAuthunticatedUser, getAllCount);
+router
+  .route("/get-all-medical-history")
+  .get(isAuthunticatedUser, getAllMedicalHistory);
+router
+  .route("/get-all-doctor-account")
+  .get(isAuthunticatedUser, getAllDoctorAccount);
+router
+  .route("/get-all-patient-account")
+  .get(isAuthunticatedUser, getAllPatientAccount);
+router
+  .route("/get-all-locaiton-clinic")
+  .get(isAuthunticatedUser, getAllLocationClinic);
 module.exports = router;
